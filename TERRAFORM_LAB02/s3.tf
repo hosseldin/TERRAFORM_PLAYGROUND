e3a0d62124145abde68b8a58ac88d5deb0862af6
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-03-16 01:30:21 +0200

    created an s3 bucket using terraform

---
+++ TERRAFORM_LAB02/s3.tf
@@ -0,0 +1,57 @@
+# ==============================================
+#
+# =========== TERRAFORM FILE START =============
+#
+# ==============================================
+#
+# Created By: Hossam Mahmoud
+# Date: 2025-03-16
+# Filename: s3.tf
+# Description: creating s3 bucket
+# Version: 1.0.0
+# Copyright (c) 2025 Hossam. All rights reserved.
+#
+# ==============================================
+#
+# ====== TERRAFORM IMPLEMENTATION START ======
+#
+# ==============================================
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "hosa-statebucket" # Change this if needed
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+
+
+# ==============================================
+#
+# ======= TERRAFORM IMPLEMENTATION END ========
+#
+# ==============================================
+#
+# End of File
+# Copyright (c) 2025 Hossam. All rights reserved.
+#
+# ==============================================
+#
+# ============= TERRAFORM FILE END =============
+#
+# ==============================================
