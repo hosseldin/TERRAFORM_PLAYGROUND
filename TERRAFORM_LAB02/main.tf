1a43973406d512b6f6355966b2a114161b57be7b
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2025-03-16 14:18:03 +0200

    removed unnecessary lines

--- TERRAFORM_LAB02/main.tf
+++ TERRAFORM_LAB02/main.tf
@@ -43,30 +43,21 @@ module "vpc" {
 }
 
 module "security_group" {
-  source = "./modules/security_group"
-
-  vpc_id = module.vpc.vpc_id
-  # vpc_id          = data.aws_vpc.selected.id
+  source          = "./modules/security_group"
+  vpc_id          = module.vpc.vpc_id
   security_groups = var.security_groups
   ingress_rules   = var.ingress_rules
   egress_rules    = var.egress_rules
 }
 
 module "instances" {
-  source    = "./modules/instance"
-  instances = var.instances
-  subnets   = module.vpc.subnets
-  # subnets        = data.aws_vpc.selected
+  source         = "./modules/instance"
+  instances      = var.instances
+  subnets        = module.vpc.subnets
   security_group = module.security_group.security-group-id
   ami            = data.aws_ami.ubuntu
 }
 
-# data "aws_vpc" "selected" {
-#   filter {
-#     name   = "tag:Name"
-#     values = [var.vpc_name]
-#   }
-# }
 
 # ==============================================
 #
